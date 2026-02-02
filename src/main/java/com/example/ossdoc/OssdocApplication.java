5608cf65da5f224b65b710493ff040ca6c00e109
Kimsejin-56 <saj5877@naver.com>
2026-02-02 19:13:22 +0900

    Init: 초기설정

---
+++ src/main/java/com/example/ossdoc/OssdocApplication.java
@@ -0,0 +1,13 @@
+package com.example.ossdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssdocApplication.class, args);
+	}
+
+}
