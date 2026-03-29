45484f6e58a4e9f6b0150941d5b991c729fe606c
JSH-12 <jsh112108@naver.com>
2026-03-29 21:40:13 +0900

    Feat: facts.json 기반 GraphStore 적재 기능 구현(#14)

--- src/main/java/com/example/ossdoc/domain/artifact/service/ArtifactContentReader.java
+++
@@ -1,8 +0,0 @@
-package com.example.ossdoc.domain.artifact.service;
-
-import com.example.ossdoc.domain.artifact.entity.Artifact;
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface ArtifactContentReader {
-    JsonNode readJson(Artifact artifact);
-}
