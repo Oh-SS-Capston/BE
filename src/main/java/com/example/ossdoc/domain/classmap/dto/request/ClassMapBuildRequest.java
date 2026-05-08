4523b6b48f82e6551adfc02e3f5c307d37b98395
ohsj <dhtmdwns319@naver.com>
2026-05-08 23:01:20 +0900

    feat : Request DTO 빌더 추가

--- src/main/java/com/example/ossdoc/domain/classmap/dto/request/ClassMapBuildRequest.java
+++ src/main/java/com/example/ossdoc/domain/classmap/dto/request/ClassMapBuildRequest.java
@@ -3,10 +3,14 @@ package com.example.ossdoc.domain.classmap.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class ClassMapBuildRequest {
     @NotBlank
